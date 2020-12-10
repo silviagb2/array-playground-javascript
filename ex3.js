33695793b4b49f622bd1a465dd9802b9202d725b
bcn-alumno <bcn-alumno@nm_madrid.local>
2020-12-10 14:04:01 +0100

    ex3

--- ex3.js
+++ ex3.js
@@ -18,7 +18,7 @@
 
 let muchos_peces = ["Dory", "Nemo", "Shipho", "Estrella"]
 
-for(let i=0; i<=muchos_peces.length; i++) {
+for(let i=0; i<muchos_peces.length; i++) {
     let siguiente_pez = muchos_peces[i]
     console.log("Pez siguiente: " + siguiente_pez)
 }
